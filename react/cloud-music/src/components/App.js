ce94d9f2878981e09241336b8c166a19839072f5
zzc <1270835582@qq.com>
2019-08-15 23:20:29 +0800

    learn

--- react/cloud-music/src/components/App.js
+++ react/cloud-music/src/components/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom'
+import {
+  BrowserRouter as Router, Route, Switch,
+  Redirect, NavLink
+} from 'react-router-dom'
+import Recommend from './recommend/Recommend'
+import Ranking from './ranking/Ranking'
+import Search from './search/Search'
 import './App.styl';
 
 function App() {
@@ -7,8 +13,33 @@ function App() {
     <Router>
       <div className="app">
         <header className="app-header">
-          Cloud Music
+          <span>Cloud Music</span>
         </header>
+        <div className="music-tab">
+          <div className="tab-item">
+            <NavLink to="/recommend" className="nav-link">
+              <span>推荐音乐</span>
+            </NavLink>
+          </div>
+          <div className="tab-item">
+            <NavLink to="/ranking" className="nav-link">
+              <span>排行榜</span>
+            </NavLink>
+          </div>
+          <div className="tab-item">
+            <NavLink to="/search" className="nav-link">
+              <span>搜索</span>
+            </NavLink>
+          </div>
+        </div>
+        <div className="music-view">
+          <Switch>
+            <Route path='/recommend' component={Recommend} />
+            <Route path='/ranking' component={Ranking} />
+            <Route path='/search' component={Search} />
+            <Redirect from='/' to='/recommend' />
+          </Switch>
+        </div>
       </div>
     </Router>
   );
---
+++ react/cloud-music/src/components/recommend/Recommend.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './recommend.styl'
+
+class Recommend extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default Recommend;
